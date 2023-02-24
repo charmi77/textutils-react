3ef379de41a53337d1e435e1fd64036064f464b7
charmi77 <charmi0705@gmail.com>
2023-02-24 22:49:39 +0530

    Updates

---
+++ precache-manifest.2f7d21c5aa88123720860a13c7753ad0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "74d1f1f994e49349f765",
+    "url": "/textutils-react/static/css/main.4bfff704.chunk.css"
+  },
+  {
+    "revision": "74d1f1f994e49349f765",
+    "url": "/textutils-react/static/js/main.fe45c3fd.chunk.js"
+  },
+  {
+    "revision": "54421935ba8a17b99e94",
+    "url": "/textutils-react/static/js/runtime~main.cd1ae7a9.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/textutils-react/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/textutils-react/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "0430e46922d7b6a5e58907a7d093fd60",
+    "url": "/textutils-react/index.html"
+  }
+];
